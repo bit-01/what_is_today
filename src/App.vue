4a4d57bf0ce176c3c665f52c3229ed45ca426b26
Bassel Aflak <b7a@hotmail.de>
2022-12-25 20:54:18 +0300

    added header and date input

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <header>
+    <nav>
+      <router-link to="/">
+        <img src="@/assets/logo.svg" id="desktop_logo" width="200" alt="What is today logo">
+      </router-link>
+    </nav>
+  </header>
   <router-view/>
 </template>
 
-
-<script>
-  import pixabay from './pixabay';
-  import { onMounted } from 'vue';
-
-  export default {
-    name: "App",
-    setup() {
-      const pixBay = pixabay();
-
-      onMounted(() => {
-        pixBay.getImageByPhrase("Red Roses")
-      })
-    }
-  }
-</script>
-
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:ital,wght@0,100;0,300;0,400;0,500;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,700;1,800;1,900&display=swap');
+
+:root {
+  --wit-white: #fff;
+  --wit-red: #ca3433;
+  --wit-gray-dark: #3f4348;
+  --wit-orange: #f9b31c;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Alegreya Sans SC', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: var(--wit-gray-dark);
+  max-width: 992px;
+  margin: auto;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+h1,h2,h3 {
+  color: var(--wit-orange);
+  text-transform: capitalize;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>what holiday is today ?</h1>
+
+    <div class="date">
+      <div>
+        <label id="date_label" for="h_date">Date</label>
+        <Datepicker v-model="h_date" name="h_date" id="h_date" aria-labelledby="date_label" :enable-time-picker="false" format="MMMM dd, yyyy"></Datepicker>
+      </div>
+
+      
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue';
+import Datepicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Datepicker
+  },
+  setup() {
+    const h_date = ref(new Date())
+
+    return {h_date}
   }
+  
 }
 </script>
+
+<style>
+.date {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, auto);
+  padding: 1em;
+  text-align: center;
+  column-gap: .5em;
+}
+.date > div {
+  font-weight: 500;
+  color: var(--wit-white);
+  background-color: var(--wit-red);
+  font-size: 1.5em;
+  border-radius: 7px;
+  padding: .3em;
+  margin: 0;
+}
+
+.dp__theme_light {
+   --dp-text-color: var(--wit-gray-dark);
+   --dp-primary-color: var(--wit-orange);
+   --dp-primary-text-color: var(--wit-white);
+   --dp-success-color: var(--wit-orange);
+   --dp-danger-color: var(--wit-red);
+   --dp-icon-color: var(--wit-red);
+}
+</style>
