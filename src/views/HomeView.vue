20cb79a664b0f3a35a744f9c8c3a8dfc7fdeebb1
Bassel Aflak <b7a@hotmail.de>
2022-12-27 19:14:22 +0300

    added holiday parts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,8 +15,29 @@
           </option>
         </select>
       </div>
+      <div>
+        <label id="type_label" for="h_label">Type</label>
+        <select :value="h_type" name="h_type" id="h_type" >
+          <option value="">All</option>
+          <option v-for="ty,k in a_types" :value="ty" :key="k">
+            {{ capitalizeWord(ty) }}
+          </option>
+        </select>
+      </div>
 
       
+    </div>
+
+    <div class="main">
+      <div class="h_container" v-for="s in 6" :key="s">
+        <img src="https://www.openconsultingcm.com/wp-content/themes/consultix/images/no-image-found-360x250.png" class="h_img" :alt="s" />
+        <div class="h_content">
+          <h2 class="h_title">Holiday Title</h2>
+          <h3 class="h_type">Type</h3>
+          <h3 class="h_country">Country</h3>
+          <p class="h_description">Some Description about the holiday will go right in here blah blah blah Some Description about the holiday will go right in here blah blah blah</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -25,8 +46,8 @@
 import { ref, onMounted } from 'vue';
 import Datepicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
-import clapi from '@/calendarific'
-import getIP from '@/ip'
+// import clapi from '@/calendarific'
+// import getIP from '@/ip'
 
 export default {
   name: 'HomeView',
@@ -36,29 +57,33 @@ export default {
   setup() {
     const h_date = ref(new Date())
     const h_country = ref('')
+    const h_type = ref('')
     const a_countries = ref([])
-    const _clapi =  clapi()
+    const a_types = ref([ 'national', 'local', 'religious', 'observance'])
+    // const _clapi =  clapi()
+
 
     onMounted(() => {
       // _clapi.getHolidays({h_date: h_date.value, country: "US"})
-      _clapi.getAllCountries().then(() => {
-        a_countries.value = _clapi.allCountries.value
-        getIP().then((data) => {  
-          h_country.value = data.country
-        })
-       
-
-      })
-      
-      
+      // _clapi.getAllCountries().then(() => {
+      //   a_countries.value = _clapi.allCountries.value
+      //   getIP().then((data) => {  
+      //     h_country.value = data.country
+      //   })
+      // })
     })
 
+    const capitalizeWord = (word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }
     
     return {
       h_date, 
       h_country, 
+      h_type,
       a_countries,
-      
+      a_types,
+      capitalizeWord
     }
   }
 
@@ -70,10 +95,10 @@ export default {
 .date {
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(4, auto);
-  padding: 1em;
   text-align: center;
   column-gap: .5em;
+  row-gap: .5em;
+  margin-bottom: 1.5em;
 }
 .date > div {
   font-weight: 500;
@@ -105,5 +130,48 @@ select {
   padding: 6px 12px;
   background: var(--wit-white);
   color: var(--wit-gray-dark);
+}
+
+.home .main {
+  display: grid;
+  padding: .5em;
+  gap: 1rem;
+  text-align: left;
+}
+
+.main .h_container {
+  box-shadow: rgba(0,0,0,.1) 0 0 20px 1px;
+  border-radius: 7px;
+}
+.h_content {
+  padding: 1rem;
+}
+.h_title, .h_country, .h_type {
+  margin: 0;
+}
+.h_type, .h_country {
+  color: var(--wit-red);
+  font-size: 12px;
+}
+.h_img {
+  width: 100%;
+  border-radius: 7px 7px 0 0;
+}
+
+@media (min-width: 992px) {
+  .date {
+    grid-template-columns: repeat(3, auto);
+
+  }
+  .home .main {
+    grid-template-columns: repeat(4, auto);
+  }
+
+}
+
+@media (min-width: 640px) and (max-width:991px) {
+  .home .main {
+    grid-template-columns: repeat(2, auto);
+  }
 }
 </style>
